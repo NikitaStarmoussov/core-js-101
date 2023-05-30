e7b917f0c30ec6cc1568bab98f1e49ab6aec291f
NikitaStarmoussov <nikta.starmousov@gmail.com>
2023-05-30 12:54:08 +0500

    feat: add all numbers task

--- src/02-numbers-tasks.js
+++ src/02-numbers-tasks.js
@@ -125,8 +125,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -140,8 +140,8 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -157,8 +157,8 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a * a + b * b + c * c);
 }
 
 /**
@@ -178,10 +178,9 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+function roundToPowerOfTen(num, pow) {
+  return pow === 0 ? num : Math.round(num / 10 ** pow) * 10 ** pow;
 }
-
 /**
  * Returns true is the number is prime; otherwise false.
  * See: https://en.wikipedia.org/wiki/Primality_test
@@ -199,8 +198,31 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+function isPrime(n) {
+  // i use my codewars solution
+  const k = n % 10;
+  const f = n <= 0 || n % 2 === 0 || n === 1 || n === 9;
+  const s = k === 0 || k === 5 || k === 4 || k === 6 || k === 8;
+  if (n === 2 || n === 5 || n === 3 || n === 7) {
+    return true;
+  }
+  if (f || s) {
+    return false;
+  }
+  if (n < 10000) {
+    for (let i = 3; i < n / 5; i += 1) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+  } else {
+    for (let i = 3; i < n / 500; i += 1) {
+      if (n % i === 0) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 /**
@@ -218,8 +240,8 @@ function isPrime(/* n */) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(/* value, def */) {
-  throw new Error('Not implemented');
+function toNumber(value, def) {
+  return parseInt(value, 10) || def;
 }
 
 module.exports = {
